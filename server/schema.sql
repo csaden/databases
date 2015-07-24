460abbb662ca02eb542f40a1d29daf39a1a0908b
csaden <csaden@gmail.com>
2015-07-24 15:10:26 -0700

    added client code and wrote schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,24 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL auto_increment,
+  message_id INT NOT NULL,
+  user_id INT NOT NULL,
+  message_text TEXT,
+  createdAt TIMESTAMP NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT NOT NULL auto_increment,
+  roomname VARCHAR(255),
+  message_id INT NOT NULL
+);
 
-
+CREATE TABLE users (
+  id INT NOT NULL auto_increment,
+  username VARCHAR(50)
+);
 
 
 /*  Execute this file from the command line by typing:
